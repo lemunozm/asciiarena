a87097effb25941fa177c9baa97aa9219cbc17a6
lemunozm <lemunozm@gmail.com>
2020-12-11 15:40:10 +0100

    Added auto client mode when user executes with no arguments.

--- src/main.rs
+++ src/main.rs
@@ -20,6 +20,13 @@ mod specification;
 use clap::{self, App, AppSettings};
 
 fn main() {
+    let mut args: Vec<String> = std::env::args().collect();
+    // If the user do not specify a subcommand, then run automatically the client mode with no args.
+    // This facilitates the usage for distributables.
+    if args.len() == 1 {
+        args.push("client".into());
+    }
+
     let matches = App::new(clap::crate_name!())
         .version(clap::crate_version!())
         .author(clap::crate_authors!())
@@ -27,7 +34,7 @@ fn main() {
         .setting(AppSettings::ArgRequiredElseHelp)
         .subcommand(client::configure_cli())
         .subcommand(server::configure_cli())
-        .get_matches();
+        .get_matches_from(args);
 
     match matches.subcommand() {
         ("client", Some(matches)) => client::run(matches),
